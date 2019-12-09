c33339c455f634ad30cba4d85fe225be5e2163f8
kkimu <kimura.kzm@gmail.com>
2019-12-09 21:12:17 +0900

    Update script

--- generate_hls.sh
+++ generate_hls.sh
@@ -3,7 +3,7 @@
 MEDIA_FILE_PATH=server/media
 
 #rm -rf ${MEDIA_FILE_PATH}
-mkdir -p ${MEDIA_FILE_PATH}
+#mkdir -p ${MEDIA_FILE_PATH}
 
 # Ref http://mobilehackerz.jp/archive/wiki/index.php?%BA%C7%BF%B7ffmpeg%A4%CE%A5%AA%A5%D7%A5%B7%A5%E7%A5%F3%A4%DE%A4%C8%A4%E1
 ffmpeg \
@@ -15,4 +15,4 @@ ffmpeg \
     -hls_time 5 \
     -hls_base_url http://localhost:8080/ \
     -hls_flags omit_endlist+append_list \
-    server/media/index.m3u8
+    ${MEDIA_FILE_PATH}/index.m3u8
